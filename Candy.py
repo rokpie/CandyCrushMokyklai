203822980ca6f2f0b073865f1c66de947ed61595
rokpie <91060774+rokpie@users.noreply.github.com>
2021-10-20 19:28:06 +0300

    Update Candy.py

--- Candy.py
+++ Candy.py
@@ -1,14 +1,15 @@
-
-lenta = []
-
-lygiai = [{lenta:[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], "dimensions" : 4}, {lenta:[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], "dimensions" : 4}]
-
+lygiai = [{'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}, {'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}]
 
 def drawBoard(lygis):
-  for y in range(lygiai[(lygis -1)].dimensions):
-    for x in range(lygiai[(lygis-1)].dimensions):
-      print(lygiai[(lygis-1)].lenta[(y)*8 + x], end= (" | "))
+  lygis  = lygis -1
+  a = "---"
+  for i in range((lygiai[lygis]['dimensions'])-1):
+    a= a+"----"
+  for y in range(lygiai[(lygis)]['dimensions']):
+    for x in range(lygiai[(lygis)]['dimensions']):
+      print(lygiai[(lygis)]['lenta'][(y)*(lygiai[(lygis)]['dimensions']) + x], end= (" | "))
     print()
-    print("-------------------------------")
+    print(a)
 
-drawBoard(1)
+level = int(input("Kurio nori lygio: ")) 
+drawBoard(level)
