8cdcbbe94ac2cbd320af1d13e00019ef748c965c
rokpie <91060774+rokpie@users.noreply.github.com>
2021-10-20 20:04:48 +0300

    Update Candy.py

--- Candy.py
+++ Candy.py
@@ -1,8 +1,10 @@
-# lygių masyvas kuriame yra visų lygių lentų reikšmės ir dimensijos.
-lygiai = [{'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}, {'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}]
+lygiai = [
+    {'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}, 
+    {'lenta':[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], 'dimensions' : 4}
+  ]
 
-def drawBoard(lygis):
-  lygis  = lygis -1
+def drawBoard():
+  lygis  = level -1
   a = "---"
   for i in range((lygiai[lygis]['dimensions'])-1):
     a= a+"----"
@@ -11,16 +13,16 @@ def drawBoard(lygis):
       print(lygiai[(lygis)]['lenta'][(y)*(lygiai[(lygis)]['dimensions']) + x], end= (" | "))
     print()
     print(a)
+  
+def langelioTikrinimas(x, y): #nuskaitys y nuo 0 iki (dimensions -1) ir x nuo 0 iki (dimensions -1)
+  lygis = level - 1
+  if (0 <= y < lygiai[lygis]['dimensions']):
+    if (0<= x < lygiai[lygis]['dimensions']):
+      return True
+    else:
+      return False
+  else:
+    return False
 
 level = int(input("Kurio nori lygio: ")) 
-drawBoard(level)
-
-
-# kvadratelių spalvos ir tų spalvų reiksmes
-oranzine = "o"
-melyna = "m"
-zalia = "z"
-raudona = "r"
-
-
-
+drawBoard()
