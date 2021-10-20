60c62591da3d9f84d6fe47d8deb7221795eaf7ec
rokpie <91060774+rokpie@users.noreply.github.com>
2021-10-20 16:35:24 +0300

    Update Candy.py

--- Candy.py
+++ Candy.py
@@ -1,13 +1,13 @@
 
 lenta = []
 
-lygiai = {lenta:[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], "dimensions" : 4}
+lygiai = [{lenta:[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], "dimensions" : 4}, {lenta:[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0], "dimensions" : 4}]
 
 
 def drawBoard(lygis):
-  for y in range(lygiai[lygis].dimensions):
-    for x in range(lygiai[lygis].dimensions):
-      print(lenta[(y)*8 + x], end= (" | "))
+  for y in range(lygiai[(lygis -1)].dimensions):
+    for x in range(lygiai[(lygis-1)].dimensions):
+      print(lygiai[(lygis-1)].lenta[(y)*8 + x], end= (" | "))
     print()
     print("-------------------------------")
 
