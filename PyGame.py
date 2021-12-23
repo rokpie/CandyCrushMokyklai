1bf3d8af4694f91d949a05ea922a06403071fed4
Uncle1Ben <martynasadrenalinas@gmail.com>
2021-12-23 21:20:44 +0200

    .

--- PyGame.py
+++ PyGame.py
@@ -1,8 +1,16 @@
-import sys, pygame
-from Candy.py import *
-pygame.init()
+import sys
+import pygame as p
+from Candy import *
+p.init()
 
 size = widhth, height = 800, 800
 
+screen = p.display.set_mode((800, 400))
+p.display.set_caption("Candy")
+
+while True:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            p.quit()
 
 
